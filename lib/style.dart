1f99013a0cbe08508fb501445f1e5648505e8af9
Aditya Ranjan <ranjanaditya263@gmail.com>
2022-02-01 19:06:13 +0530

    style file added

---
+++ lib/style.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color backgroundColor0 = const Color(0xffFCD5C7);
+Color backgroundColor1 = const Color(0xffE5EBED);
+Color backgroundColor2 = const Color(0xffDCD1EF);
+
+Color whiteColor = const Color(0xffFBFCFD);
+Color blackColor = const Color(0xff252D48);
+Color userShadow = const Color(0xff12637D);
+
+Color partColor0 = const Color(0xffF4DBE6);
+Color partColor1 = const Color(0xffFFB987);
+Color partColor2 = const Color(0xffFF78A9);
+Color partColor3 = const Color(0xffDA8AFF);
+
+TextStyle UserStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  fontSize: 40,
+  color: blackColor,
+);
+
+TextStyle TopicStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontWeight: FontWeight.w700,
+  fontSize: 30,
+);
+
+TextStyle UserTopicStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontWeight: FontWeight.w700,
+  fontSize: 20,
+);
+
+TextStyle UserSubtitleStyle = GoogleFonts.poppins(
+  fontSize: 18,
+  color: blackColor.withOpacity(0.5),
+);
+
+TextStyle UserOptionsStyle = GoogleFonts.poppins(
+  fontSize: 16,
+  color: blackColor,
+);
